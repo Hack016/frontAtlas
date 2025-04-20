60f2947eed4b46655ce00fb2f2c96d12ca299fff
Hack0b <jacobo.erouco@udc.es>
2025-04-20 22:43:08 +0200

    Editado TrainFeedScreen para que tenga paginación y mejorar así la interfaz del usuario y comienzo del profileScreen mostrando sesiones

--- pruebaApp/components/home/ProfileFeedScreen.jsx
+++ pruebaApp/components/home/ProfileFeedScreen.jsx
@@ -1,12 +1,167 @@
-import { Text, StyleSheet } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  RefreshControl,
+  Image,
+  ActivityIndicator,
+} from "react-native";
+import { useFetchWithAuth } from "../../utils/fetchWithAuth";
+import { useNavigation } from "@react-navigation/native";
+import { BASE_URL } from "../../context/config";
+import Titan from "../../assets/TitanVectorizado.png";
 
 export default function ProfileFeedScreen() {
-  return <Text style={styles.text}>Profile</Text>;
+  const navigation = useNavigation();
+
+  const fetchWithAuth = useFetchWithAuth();
+
+  const [profileData, setProfileData] = React.useState(null);
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const fetchProfileData = async () => {
+    try {
+      setRefreshing(true);
+      console.log(BASE_URL);
+      const response = await fetchWithAuth(`${BASE_URL}api/profile/`, {
+        method: "GET",
+      });
+
+      setRefreshing(false);
+
+      const data = await response.json();
+      setProfileData(data);
+
+      if (response.ok) {
+        return { success: true, data };
+      } else {
+        return { success: false, error: data.error || "Server not available" };
+      }
+    } catch (error) {
+      return { success: false, error };
+    }
+  };
+
+  React.useEffect(() => {
+    fetchProfileData();
+  }, []);
+
+  if (!profileData) {
+    return (
+      <SafeAreaView
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <ActivityIndicator color="white" size="small" />
+      </SafeAreaView>
+    );
+  }
+  // if (!profileData) return null;
+
+  // const { usuario, estadisticas } = profileData;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView
+        contentContainerStyle={styles.scrollContainer}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={fetchProfileData}
+          />
+        }
+      >
+        <Image
+          source={
+            profileData.usuario?.foto_perfil
+              ? { uri: profileData.usuario.foto_perfil }
+              : Titan
+          }
+          style={styles.profileImage}
+        />
+        <Text style={styles.name}>
+          {profileData.usuario?.nombre
+            ? profileData.usuario.nombre
+            : "No tiene nombre"}
+        </Text>
+        <Text style={styles.username}>
+          @
+          {profileData.usuario?.username
+            ? profileData.usuario.username
+            : "No tiene username"}
+        </Text>
+
+        {profileData.usuario?.biografia ? (
+          <Text style={styles.bio}>{profileData.usuario.biografia}</Text>
+        ) : null}
+
+        <View style={styles.statsContainer}>
+          <View style={styles.statBox}>
+            <Text style={styles.statNumber}>
+              {profileData.estadisticas.total_sesiones}
+            </Text>
+            <Text style={styles.statLabel}>Sesiones</Text>
+          </View>
+          <View style={styles.statBox}>
+            <Text style={styles.statNumber}>
+              {profileData.estadisticas.total_seguidores}
+            </Text>
+            <Text style={styles.statLabel}>Seguidores</Text>
+          </View>
+          <View style={styles.statBox}>
+            <Text style={styles.statNumber}>
+              {profileData.estadisticas.total_seguidos}
+            </Text>
+            <Text style={styles.statLabel}>Seguidos</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 20,
+  scrollContainer: {
+    alignItems: "center",
+    paddingVertical: 20,
+  },
+  profileImage: {
+    width: 110,
+    height: 110,
+    borderRadius: 55,
+    marginBottom: 15,
+  },
+  name: {
+    fontSize: 22,
     fontWeight: "bold",
+  },
+  username: {
+    fontSize: 16,
+    color: "gray",
+    marginBottom: 10,
+  },
+  bio: {
+    fontSize: 14,
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  statsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    marginTop: 20,
+  },
+  statBox: {
+    alignItems: "center",
+  },
+  statNumber: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  statLabel: {
+    fontSize: 14,
+    color: "gray",
   },
 });
--- pruebaApp/components/home/TrainFeedScreen.jsx
+++ pruebaApp/components/home/TrainFeedScreen.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
   ScrollView,
+  FlatList,
   StyleSheet,
   View,
   Text,
@@ -13,6 +14,8 @@ import { useFetchWithAuth } from "../../utils/fetchWithAuth";
 import { useNavigation } from "@react-navigation/native";
 import { BASE_URL } from "../../context/config";
 
+const LIMIT = 20; // Define cuantos ejs se cargan por página
+
 export default function TrainFeedScreen() {
   const navigation = useNavigation();
 
@@ -20,73 +23,132 @@ export default function TrainFeedScreen() {
 
   const [exercises, setExercises] = React.useState([]);
   const [refreshing, setRefreshing] = React.useState(false);
+  const [offset, setOffset] = React.useState(0);
+  const [loadingMore, setLoadingMore] = React.useState(false);
+  const [hasMore, setHasMore] = React.useState(true);
 
-  const fetchExercises = async () => {
+  const fetchExercises = async (initial = false) => {
     try {
-      setRefreshing(true);
-      console.log(BASE_URL);
-      const response = await fetchWithAuth(`${BASE_URL}api/EjercicioFeed/`, {
-        method: "GET",
-      });
+      if (loadingMore || (!initial && !hasMore)) return;
 
-      setRefreshing(false);
+      if (initial) {
+        setOffset(0);
+        setHasMore(true);
+        setRefreshing(true);
+      } else {
+        setLoadingMore(true);
+      }
+
+      const response = await fetchWithAuth(
+        `${BASE_URL}api/EjercicioFeed/?limit=${LIMIT}&offset=${initial ? 0 : offset}`
+      );
 
       const data = await response.json();
-      setExercises(data);
 
       if (response.ok) {
-        return { success: true, data };
+        if (initial) {
+          setExercises(data.results);
+        } else {
+          // setExercises((prev) => [...prev, ...data.results]);
+          setExercises((prev) => {
+            //Evita agregar ejercicios duplicados
+            const newItems = data.results.filter(
+              (item) => !prev.some((e) => e.idEjercicio === item.idEjercicio)
+            );
+            return [...prev, ...newItems];
+          });
+        }
+
+        setOffset((prev) => prev + LIMIT);
+        if (data.results.length < LIMIT) setHasMore(false); // No hay más datos
       } else {
-        return { success: false, error: data.error || "Server not available" };
+        console.error("Failed: server error");
       }
     } catch (error) {
-      return { success: false, error };
+      console.error("Failed to fetch exercises", error);
+    } finally {
+      setLoadingMore(false);
+      setRefreshing(false);
     }
   };
 
   React.useEffect(() => {
-    fetchExercises();
+    fetchExercises(true);
   }, []);
+
+  const renderItem = (
+    { item } // Renderiza cada ejercicio, lo hago en función a parte para que el código esté más limpio
+  ) => (
+    <View style={styles.cardEnvelope}>
+      <Pressable
+        style={styles.card}
+        onPress={() =>
+          navigation.navigate("ExerciseDetail", {
+            idEjercicio: item.idEjercicio,
+            nombreEjercicio: item.nombre, // Le paso el nombre para que el titulo de la página sea el nombre del ejercicio
+          })
+        }
+      >
+        {item.imagen && (
+          <Image source={{ uri: item.imagen }} style={styles.image} />
+        )}
+        <View style={styles.textContainer}>
+          <Text style={styles.exerciseName}>{item.nombre}</Text>
+          <Text style={styles.muscleGroup}>
+            Músculos: {item.musculos_principales.join(", ")}
+          </Text>
+        </View>
+      </Pressable>
+    </View>
+  );
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView
+      {/* <ScrollView
+        onScroll={({ nativeEvent }) => {
+          const { layoutMeasurement, contentOffset, contentSize } = nativeEvent;
+          const isCloseToBottom = // variable para hacer scroll infinito
+            layoutMeasurement.height + contentOffset.y >=
+            contentSize.height - 50;
+
+          if (isCloseToBottom) {
+            fetchExercises(); // Carga más si está cerca del final
+          }
+        }}
+        scrollEventThrottle={400}
         contentContainerStyle={styles.scrollContainer}
         refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={fetchExercises} /> // RefresControl es para deslizar hacia abajo y recargar
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={() => fetchExercises(true)}
+          />
         }
       >
-        {exercises.map((item) => (
-          <Pressable
-            key={item.idEjercicio}
-            style={styles.card}
-            onPress={() =>
-              navigation.navigate("ExerciseDetail", {
-                idEjercicio: item.idEjercicio,
-                nombreEjercicio: item.nombre, // Le paso el nombre para que el titulo de la página sea el nombre del ejercicio
-              })
-            }
-          >
-            {item.imagen && (
-              <Image source={{ uri: item.imagen }} style={styles.image} />
-            )}
-            <View style={styles.textContainer}>
-              <Text style={styles.exerciseName}>{item.nombre}</Text>
-              <Text style={styles.muscleGroup}>
-                Músculos: {item.musculos_principales.join(", ")}
-              </Text>
-            </View>
-          </Pressable>
-        ))}
-      </ScrollView>
+        {exercises.map((item) => renderItem({ item }))}
+      </ScrollView> */}
+      <FlatList
+        data={exercises}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.idEjercicio.toString()}
+        onEndReached={() => fetchExercises(false)}
+        onEndReachedThreshold={0.5} // Carga más cuando estás al 50% del final
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={() => fetchExercises(true)}
+          />
+        }
+        contentContainerStyle={styles.scrollContainer}
+      />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollContainer: {
+  cardEnvelope: {
+    alignSelf: "stretch",
+    flexDirection: "row",
     alignItems: "center",
-    paddingVertical: 20,
   },
   card: {
     flexDirection: "row",
